118252336f02400efb4937a49a5407bd5b905b78
rohan057 <210074349@aston.ac.uk>
2025-01-13 18:35:17 +0000

    Hotline Page
    Added layout with a few contacts and updated css

--- hotline.php
+++ hotline.php
@@ -21,9 +21,34 @@
             <li><a href="login.php">Login</a></li>
             <li><a href="journal.php">Journal</a></li>
             <li><a href="goals.php">Goals</a></li>
-                </ul>
-            </li>
         </ul>
     </nav>
+
+    <div class="container">
+    <h1 class="hotline-header">Hotline Numbers</h1>
+    <table class="hotline-table">
+        <tr>
+            <th>Type of Emergency</th>
+            <th>Number/Website</th>
+        </tr>
+        <tr>
+            <td>Ambulance</td>
+            <td>999</td>
+        </tr>
+        <tr>
+            <td>Suicide Prevention</td>
+            <td><a href="https://sossilenceofsuicide.org/">0808 115 1505</a></td>
+        </tr>
+        <tr>
+            <td>Domestic Violence</td>
+            <td><a href="https://www.nationaldahelpline.org.uk/">0808 2000 247</a></td>
+        </tr>
+        <tr>
+            <td>Mental Health Support</td>
+            <td><a href="https://www.samaritans.org/">116 123</a></td>
+        </tr>
+    </table>
+</div>
+
 </body>
 </html>
