56cf7a3ff71064847dd444c83fb5452a90a748f0
rohan057 <210074349@aston.ac.uk>
2025-01-30 17:14:22 +0000

    Adding Content
    Embedded videos onto page and added references

--- exercise-videos.php
+++ exercise-videos.php
@@ -38,20 +38,84 @@ session_start();
     <div class="video-container">
         <div class="video-grid">
             <div class="video-item">
-                <h3>Full Body Workout</h3>
-                <iframe src="https://www.youtube.com/embed/VIDEO_ID_1" frameborder="0" allowfullscreen></iframe>
+                <h3>Home Workout For Beginners</h3>
+                <iframe src="https://www.youtube.com/embed/2fCKd-BQOhw" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=2fCKd-BQOhw -->
             </div>
             <div class="video-item">
-                <h3>Leg Strength Training</h3>
-                <iframe src="https://www.youtube.com/embed/VIDEO_ID_2" frameborder="0" allowfullscreen></iframe>
+                <h3>Full Body Beginner Workout</h3>
+                <iframe src="https://www.youtube.com/embed/Oe5CDt3dp8Q" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=Oe5CDt3dp8Q -->
             </div>
             <div class="video-item">
-                <h3>Core and Abs Routine</h3>
-                <iframe src="https://www.youtube.com/embed/VIDEO_ID_3" frameborder="0" allowfullscreen></iframe>
+                <h3>Full Body Home Workout</h3>
+                <iframe src="https://www.youtube.com/embed/zh1Kq3Qv-2U" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=zh1Kq3Qv-2U -->
             </div>
             <div class="video-item">
-                <h3>Upper Body Strength</h3>
-                <iframe src="https://www.youtube.com/embed/VIDEO_ID_4" frameborder="0" allowfullscreen></iframe>
+                <h3>ABS For Beginners</h3>
+                <iframe src="https://www.youtube.com/embed/3p8EBPVZ2Iw" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=3p8EBPVZ2Iw -->
+            </div>
+            <div class="video-item">
+                <h3>Home Leg Workout</h3>
+                <iframe src="https://www.youtube.com/embed/q7rCeOa_m58" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=q7rCeOa_m58 -->
+            </div>
+            <div class="video-item">
+                <h3>Full Body Cardio Workout</h3>
+                <iframe src="https://www.youtube.com/embed/79cx5vmf3Qg" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=79cx5vmf3Qg -->
+            </div>
+            <div class="video-item">
+                <h3>Home Chest Workout</h3>
+                <iframe src="https://www.youtube.com/embed/BkS1-El_WlE" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=BkS1-El_WlE -->
+            </div>
+            <div class="video-item">
+                <h3>Biceps, Triceps and Shoulders</h3>
+                <iframe src="https://www.youtube.com/embed/M42-2wgRWkA" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=M42-2wgRWkA -->
+            </div>
+            <div class="video-item">
+                <h3>Beginners Arm Workout</h3>
+                <iframe src="https://www.youtube.com/embed/J1-SDv_kwiY" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=J1-SDv_kwiY -->
+            </div>
+            <div class="video-item">
+                <h3>Home Back Workout</h3>
+                <iframe src="https://www.youtube.com/embed/QqZV3JEoOlc" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=QqZV3JEoOlc -->
+            </div>
+            <div class="video-item">
+                <h3>Pilates Workout</h3>
+                <iframe src="https://www.youtube.com/embed/C2HX2pNbUCM" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=C2HX2pNbUCM -->
+            </div>
+            <div class="video-item">
+                <h3>Beginner Calisthenics Workout</h3>
+                <iframe src="https://www.youtube.com/embed/kuUZYUBHryw" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=kuUZYUBHryw -->
+            </div>
+            <div class="video-item">
+                <h3>HIIT/Cardio Workout</h3>
+                <iframe src="https://www.youtube.com/embed/edIK5SZYMZo" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=edIK5SZYMZo -->
+            </div>
+            <div class="video-item">
+                <h3>Yoga For Beginners</h3>
+                <iframe src="https://www.youtube.com/embed/T41mYCmtWls" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=T41mYCmtWls -->
+            </div>
+            <div class="video-item">
+                <h3>Gentle Yoga For Beginners</h3>
+                <iframe src="https://www.youtube.com/embed/3X0hEHop8ec" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=3X0hEHop8ec -->
+            </div>
+            <div class="video-item">
+                <h3>Full Body Yoga</h3>
+                <iframe src="https://www.youtube.com/embed/Eml2xnoLpYE" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=Eml2xnoLpYE -->
+            </div>
+            <div class="video-item">
+                <h3>Core Workout</h3>
+                <iframe src="https://www.youtube.com/embed/stuPkzWMdIg" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=stuPkzWMdIg -->
+            </div>
+            <div class="video-item">
+                <h3>Shoulder Workout</h3>
+                <iframe src="https://www.youtube.com/embed/jBPdbt0Fxb4" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=jBPdbt0Fxb4 -->
+            </div>
+            <div class="video-item">
+                <h3>HIIT Workout</h3>
+                <iframe src="https://www.youtube.com/embed/g3CiXrjJ7cs" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=g3CiXrjJ7cs -->
+            </div>
+            <div class="video-item">
+                <h3>Lower Body Workout</h3>
+                <iframe src="https://www.youtube.com/embed/6atz9eroPBs" frameborder="0" allowfullscreen></iframe> <!-- https://www.youtube.com/watch?v=6atz9eroPBs -->
             </div>
         </div>
     </div>
