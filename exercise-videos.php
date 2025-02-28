77a719aa39d727df233443e83b2fa982dec03800
rohan057 <210074349@aston.ac.uk>
2025-02-28 15:55:58 +0000

    Quick Fixes

--- exercise-videos.php
+++ exercise-videos.php
@@ -9,7 +9,6 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercise Videos</title>
     <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="css/exercise-videos.css">
 </head>
 <body>
     <nav>
